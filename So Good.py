5d252e9a575d8928b77bc1ae640f60bcb1467eb9
gamer123566 <asdfhjkl9009@gmail.com>
2025-09-08 19:31:16 -0300

    Add files via upload

---
+++ So Good.py
@@ -0,0 +1,32 @@
+import random
+
+# texto == string
+
+# numero = int / float
+
+import random
+
+azar = random.randrange(0,7)
+
+azar = str(azar)
+
+1 == "wuawhd"
+2 == "yay"
+3 == "erm what the sigma"
+4 == "dude"
+5 == "oaiwjduawoijd"
+6 == "im running out of ideas"
+7 == "AAAAA"
+0 == "lalalala"
+
+
+awhdi = input("Hi there. Would you like to sign my petition?")
+if(awhdi == "yea"):
+    with open(azar + ".txt","w") as archivo:
+
+     #contenido
+
+     archivo.write("wah wah")
+
+
+
